7507744913e5ef970b98a6b37230f7c20d5429ed
Sheila <s.hopper01110@gmail.com>
2019-02-27 17:47:04 -0500

    added cohort table

---
+++ migrations/20190227171632_create.js
@@ -0,0 +1,19 @@
+//changes applied to the DB
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('cohorts',
+    function(tbl) {
+        tbl.increments();
+  
+        tbl
+            .string('name', 128)
+            .notNullable()
+
+        tbl.timestamps(true, true)
+
+    })
+
+};
+//undo
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('cohorts')
+};
